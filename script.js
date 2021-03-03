9e18b5e42abbe1004165240d48f19b9479887cef
rashda <rashda@cyprus.com>
2021-03-03 23:45:33 +0530

    forth commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    t= document.getElementById("years").value;
+    si=(p*r*t)/100;
+    document.getElementById("result").innerHTML=si;
 }
         
